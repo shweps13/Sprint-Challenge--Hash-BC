f9807e86279c92adaa1ba5ebdc6d38349ae7c6eb
Heorhii Siburov <g.siburov@gmail.com>
2020-02-14 09:09:59 -0800

    ex1 done

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,9 +1,7 @@
 #  Hint:  You may not need all of these.  Remove the unused functions.
 from hashtables import (HashTable,
                         hash_table_insert,
-                        hash_table_remove,
-                        hash_table_retrieve,
-                        hash_table_resize)
+                        hash_table_retrieve)
 
 """
 input: weights = [ 4, 6, 10, 15, 16 ], length = 5, limit = 21
@@ -34,8 +32,24 @@ def get_indices_of_item_weights(weights, length, limit):
     index = 0
 
     for weight in weights:
-        difference = limit - weights
-        print("-==", index, "weight: ", weight, "difference = ", difference, "==-")
+        # Find the difference
+        difference = limit - weight
+        # print("-==", index, "weight: ", weight, "difference = ", difference, "==-")
+
+        # Check difference in hash table to check a key
+        check_ht = hash_table_retrieve(ht, difference)
+        # it will give value of key
+        # [ 4,    6,    10,    15,    16 ]
+        # [4-0] [6-1] [10-2] [15-3] [16-4]
+
+        if check_ht is not None:
+            print("inside", index, check_ht )
+            return(index, check_ht)
+
+        hash_table_insert(ht, weight, index)
+
+        # print(ht)
+        index += 1
 
     return None
 
